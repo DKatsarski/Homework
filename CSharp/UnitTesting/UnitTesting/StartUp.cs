bb4cfcd96cb832b77de4a67ab9e8dfcfc759a0d4
DKatsarski <dd.katsarski@gmail.com>
2016-07-29 18:05:41 +0300

    Initial Commit!

---
+++ CSharp/UnitTesting/UnitTesting/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
