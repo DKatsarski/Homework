3e064e6b8b7b390b01a982c789d45617c127137e
DKatsarski <dd.katsarski@gmail.com>
2016-08-07 22:19:52 +0300

    Started Work on Cosmetics.

---
+++ WorkShops/1. KPK/Cosmetics/Solution/Cosmetics.Tests/Engine/EngineCommandTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Tests.Engine
+{
+    [TestFixture]
+    class EngineCommandTest
+    {
+        [Test]
+        public void 
+    }
+}
+/*https://github.com/TelerikAcademy/Unit-Testing/tree/master/Topics/05.%20Workshop(Students)
+*/
