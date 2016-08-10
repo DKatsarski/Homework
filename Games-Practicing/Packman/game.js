e600dab4d09922c6560fd9145ac532b8bc1a3c8a
DKatsarski <dd.katsarski@gmail.com>
2016-08-10 03:46:15 +0300

    Practice on Packman began.

---
+++ Games-Practicing/Packman/game.js
@@ -0,0 +1,9 @@
+// widnow document console
+"use strict";
+
+function createGame(selector) {
+    var canvas = document.querySelector(selector),
+    ctx = canvas.getContext("2d");
+
+    ctx.arc(50, 50, )
+}
