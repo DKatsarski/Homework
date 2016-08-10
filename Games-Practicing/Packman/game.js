984bc906357755961af0e5a86a98d847a38ed4e1
DKatsarski <dd.katsarski@gmail.com>
2016-08-10 18:05:59 +0300

    Packman updated so that he can move and his mouth opens accordingly.

--- Games-Practicing/Packman/game.js
+++ Games-Practicing/Packman/game.js
@@ -4,15 +4,42 @@
 function createGame(selector) {
     var canvas = document.querySelector(selector),
     ctx = canvas.getContext("2d"),
-    isMouthOpen = false;
+    isMouthOpen = false,
+    packman = {
+        "x": 30,
+        "y": 30,
+        "size": 30
+    },
+    dir = 0,
+    // doing the controlers (keys for movement)
+    keyCodeToDirs = {
+        "37": 2,
+        "38": 3,
+        "39": 0,
+        "40": 1, 
 
-var x = 150;
+    },
+    // a variable to make packman move in th corresponding direction
+    dirDeltas = [{
+        "x": +1,
+        "y": 0
+    }, {
+           "x": 0,
+        "y": +1
+    }, {
+        "x": -1,
+        "y": 0
+    }, {
+        "x": 0,
+        "y": -1
+    }];
+
 
 //logic to slow down opening and closing of the mouth
 var steps = 0;
 const stepstToChangeMouth = 12;
 
-    function gameLoop() {
+function gameLoop() {
     //Give a color to the packman.
     ctx.fillStyle = "yellow";
     
@@ -23,16 +50,18 @@ const stepstToChangeMouth = 12;
     ctx.beginPath();
 
     if (isMouthOpen){
+        var deltaRadians = dir * Math.PI / 2; // this is a logic to change the direction (mouth) of packman.
  // for making a circle
-    ctx.arc(x, 150, 70, Math.PI / 4, 7 * Math.PI / 4 ); 
+    ctx.arc(packman.x, packman.y, packman.size, deltaRadians + Math.PI / 4, deltaRadians + 7 * Math.PI / 4 ); 
 
     //makes a line between the last point that i've been to to the new given point'
-    ctx.lineTo(x, 150 ); 
+    //with this we can make for example a plain to shoot targets wit lasers or smth.
+    ctx.lineTo(packman.x, packman.y ); 
     } else {
-    ctx.arc(x, 150, 70, 0, 2 * Math.PI ); 
+    ctx.arc(packman.x, packman.y, packman.size, 0, 2 * Math.PI ); 
     }
    
-   x += 1;
+
     ctx.fill();
    
    steps += 1;
@@ -41,11 +70,28 @@ const stepstToChangeMouth = 12;
        
    }
 
+    packman.x += dirDeltas[dir].x;  // changing the directions appropriatly
+    packman.y += dirDeltas[dir].y;
+
     // with this method we should make the anmation, because setTimeOut coould make problems
     //if for example our machin crashes *(i would continue running and so it would make occurred)
     window.requestAnimationFrame(gameLoop); 
     }
-    return {
+document.body.addEventListener("keydown", function(ev){
+    console.log(ev.keyCode); //.keyCode shows what key have you pressed.
+
+    //a way to deal with the undefined, if the wrong key is pressed
+    //with hasOwnProperty we are avoiding the problem of when pressing zero to show undefined
+    if (!keyCodeToDirs.hasOwnProperty(ev.keyCode)) {
+        return;
+    }
+
+    //metho to change the direction
+    dir = keyCodeToDirs[ev.keyCode];
+    console.log(dir);
+});
+
+return {
         "start": function() {
             gameLoop();
         }
