f0b9a0c3949993fbebd8ca28c3eba16259554c88
DKatsarski <dd.katsarski@gmail.com>
2016-08-14 21:26:43 +0300

    First task done! Comments added. Logic for search "engine" in the site available on row 40 - 50;

--- ExamPractice/04. JavaScript DOM UI/task-1 17 June 2014/task-resources/scripts.js
+++ ExamPractice/04. JavaScript DOM UI/task-1 17 June 2014/task-resources/scripts.js
@@ -1,73 +1,118 @@
 function createImagesPreviewer(selector, items) {
+ // TODO: Check selector, check items, check items length
+  // tuk prawim selector
   var root = document.querySelector(selector);
-  root.className += ' image-previewer';
-  
-  var table = document.createElement('table');
-  table.style.height = '400px';
-  table.style.overflow = 'hidden';
-  
-  var theRow = document.createElement('tr');
-  table.appendChild(theRow);
-  root.appendChild(table);
-  
-  /* Left panel */
-  var leftPanel = document.createElement('td');  
-  leftPanel.className += ' left-panel';
+
+  var fragment = document.createDocumentFragment();
+
+  //tuk e razpredelenioeto ot lqwo i ot dqsno.
+  var imagePreviewer = document.createElement('div');// za da elementite na mqstoto si se prawqt ezi diwowe
+  imagePreviewer.style.display = 'inline-block'; //stilizirame gi
+  imagePreviewer.style.width = '75%';
+  imagePreviewer.style.float = 'left';
+  imagePreviewer.style.textAlign = 'center';
+
+  var aside = document.createElement('div');
+  aside.style.display = 'inline-block';
+  aside.style.width = '25%';
+  aside.style.textAlign = 'center';
+
+  var selectedParent = document.createElement('div');//prawmi paretn nz oshte zashto/ maj zashtoto trqbwa da dobawim klas
+  selectedParent.classList.add('image-preview');
+  var selectedImageHeader = document.createElement('h3'); // tuk prawim zalgaweito an drugata golqma kartinkata
+  selectedImageHeader.innerText = items[0].title;
+  var selectedImage = document.createElement('img'); //prawim snimkata
+  selectedImage.src = items[0].url;
+  selectedImage.style.width = '80%';
+
+  selectedParent.appendChild(selectedImageHeader);
+  selectedParent.appendChild(selectedImage);// dobawqme elementite w tozi element i po dolu dobawqme elementa kym drugiq
+  imagePreviewer.appendChild(selectedParent);
+
+
+  //syzdawame input
+  var input = document.createElement('input');
+  var inputHeader = document.createElement('h3'); // tuk syzdawame headyer 
+  inputHeader.innerText = 'Filter'; // tuk postawqme bulwite nad input-a
+  input.addEventListener('keyup', function (ev) {
+    var text = ev.target.value;
+
+    var liChildern = listOfItems.getElementsByTagName('li');
+    for (var i = 0, len = liChildern.length; i < len; i++) {
+      var currentLi = liChildern[i];
+      var header = currentLi.firstElementChild.innerText;
+      if (header.toLowerCase().indexOf(text.toLowerCase()) >= 0) { // TUK PRAWI REALNO TYRSACHKATA 
+        currentLi.style.display = 'block';
+      }
+      else {
+          currentLi.style.display = 'none';
+      }
+    }
+
+  }, false)
+
+  var listOfItems = document.createElement('ul'); // tuk zapochwame logikata, za da dobawim elementite
+  listOfItems.style.listStyleType = 'none'; // da nqmat tochki pred elementite ot lista 
+  listOfItems.style.height = '500px';
+  listOfItems.style.overflowY = 'scroll';// tuk slagame srolera po y koordinata
+  listOfItems.addEventListener('click', function (ev) {
+      var target = ev.target;
+      if (target.tagName === 'IMG') {
+        var header = target.previousElementSibling.innerText; // tuk namirame zaglawieto i kartinakta, za da q ugolemim
+                                                                // ot lqvo
+        var src = target.src;
+        selectedImageHeader.innerText = header;
+        selectedImage.src = src;
+      };
+  }, false);
+
+  listOfItems.addEventListener('mouseover', function (ev){
+      var target = ev.target;
+      if (target.tagName === 'IMG') {
+        target.parentElement.style.backgroundColor = 'gray';
+        target.parentElement.style.cursor = 'pointer';
+      }
+
+  }, false);
+
+  listOfItems.addEventListener('mouseout', function (ev) {
+    var target = ev.target;
+    if (target.tagName === 'IMG') {
+      target.parentElement.style.backgroundColor = 'initial';
+    }
+  }, false)
+
+  var li = document.createElement('li'); // dyzdawame li
+  li.classList.add('image-container');
+  var imageHeader = document.createElement('h3');
+  var image = document.createElement('img');
+  image.style.width = '90%';
+  for (var i = 0, len = items.length; i < len; i += 1) // tuk trqbwa da klonirame headyer, kartinkata i da im setnem neshtata
+  {
+    var currentItem = items[i];
+    var currentLi = li.cloneNode(true);
+    var currentImageHeader = imageHeader.cloneNode(true);
+    currentImageHeader.innerText = currentItem.title; // tuk popylawame teksta w Header-a 
+    var currentImage = image.cloneNode(true);
+    currentImage.src = currentItem.url; // tuk dobawqme socrsa na snimkata ot obekta w HTML-a
+
+    currentLi.appendChild(currentImageHeader);
+    currentLi.appendChild(currentImage);
     
-  var title = document.createElement('h1');
-  title.innerHTML = items[0].title;
-  
-  var bigImage = document.createElement('img');
-  bigImage.src = items[0].url;
-  
-  leftPanel.appendChild(title);
-  leftPanel.appendChild(bigImage);    
-  /* End of left panel */    
-  
-  /* Right panel */
-  var rightPanel = document.createElement('td');
-  rightPanel.className += ' right-panel';
-  rightPanel.style.textAlign = 'center';
-  rightPanel.style.overflowY = 'scroll';
 
-  var filter = document.createElement('div');
-  filter.className = 'filter';
-  var filterTitle = document.createElement('span');
-  filterTitle.innerHTML = 'Filter';
-  var filterBox = document.createElement('input');
-  filterBox.type = 'text';
-  filter.appendChild(filterTitle);
-  filter.appendChild(document.createElement('br'));
-  filter.appendChild(filterBox);
-
-  rightPanel.appendChild(filter);
-
-  var imgsList = document.createElement('ul');
-  imgsList.style.listStyleType = 'none';
-  imgsList.style.height = '200px';
-  for (var i = 0; i < items.length; i += 1) {
-    
-    var imgItem = document.createElement('li');
-    imgItem.setAttribute('data-index', i + '');
-    imgItem.style.width = '150px';
-
-    var imgItemTitle = document.createElement('span');
-    imgItemTitle.innerHTML = items[i].title;
-    imgItem.appendChild(imgItemTitle);
-
-    var imgItemImg = document.createElement('img');
-    imgItemImg.src = items[i].url;
-    imgItemImg.style.maxWidth = '100%';
-    
-    imgItem.appendChild(imgItemImg);   
-    imgsList.appendChild(imgItem);
+    listOfItems.appendChild(currentLi);
   }
 
-  rightPanel.appendChild(imgsList);
-  
-  /* End of right panel */
+
+  aside.appendChild(inputHeader); // pirkachwame filtyra
+
+  aside.appendChild(input); // tuk prikachwame input (tam kydeto pishem tekst) kym dqsnata strana. 
+  aside.appendChild(listOfItems);
 
 
-  theRow.appendChild(leftPanel);
-  theRow.appendChild(rightPanel);
+  fragment.appendChild(imagePreviewer);
+  fragment.appendChild(aside);
+
+  root.appendChild(fragment);
+
 }
