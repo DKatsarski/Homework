b3108295b5582f9cf113c6f021b655bb28c02b2e
DKatsarski <dd.katsarski@gmail.com>
2016-08-29 15:43:39 +0300

    Classes separated.

--- CSharp/05. High Quality Code Part 1/Events/Events/Program.cs
+++ CSharp/05. High Quality Code Part 1/Events/Events/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Events;
+using System;
 using System.Collections.Generic;
 
 
@@ -6,52 +7,6 @@ using System.Linq;
 using System.Text;
 using Wintellect.PowerCollections;
 
-class Event : IComparable
-{
-    public DateTime date;
-    public String title;
-    public String location;
-
-    public Event(DateTime date, String title, String location)
-    {
-        this.date = date;
-
-        this.title = title;
-        this.location = location;
-    }
-
-    public int CompareTo(object obj)
-    {
-        Event other = obj as Event;
-        int byDate = this.date.CompareTo(other.date);
-        int byTitle = this.title.CompareTo(other.title);
-
-        int byLocation = this.location.CompareTo(other.location);
-        if (byDate == 0)
-        {
-            if (byTitle == 0)
-                return byLocation;
-            else
-            { return byTitle; }
-
-
-        }
-        else
-            return byDate;
-    }
-
-    public override string ToString()
-    {
-        StringBuilder toString = new StringBuilder();
-        toString.Append(date
-            .ToString("yyyy-MM-ddTHH:mm:ss"));
-
-
-        toString.Append(" | " + title);
-        if (location != null && location != "") toString.Append(" | " + location);
-        return toString.ToString();
-    }
-}
 
 class Program
 {
@@ -59,81 +14,7 @@ class Program
 
     static StringBuilder output = new StringBuilder();
 
-    static class Messages
-    {
-        public static void EventAdded()
-        { output.Append("Event added\n"); }
-        public static void EventDeleted(int x)
-        {
-            if (x == 0) NoEventsFound();
-
-            else output.AppendFormat("{0} events deleted\n", x);
-        }
-        public static void NoEventsFound() { output.Append("No events found\n"); }
-        public static void PrintEvent(Event eventToPrint)
-        {
-
-
-            if (eventToPrint != null)
-            {
-                output.Append(eventToPrint + "\n");
-            }
-        }
-    }
-
-    class EventHolder
-    {
-        MultiDictionary<string, Event>
-            byTitle = new MultiDictionary<string,
-            Event>(true);
-        OrderedBag<Event>
-            byDate = new OrderedBag<
-            Event>();
-
-        public void AddEvent(DateTime date,
-                             string title,
-                              string location)
-        {
-            Event newEvent = new Event(date,
-                                       title,
-                                       location);
-            byTitle.Add(
-                        title.ToLower(),
-                        newEvent
-                        );
-            byDate.Add(newEvent); Messages.EventAdded();
-        }
-
-        public void DeleteEvents(string titleToDelete)
-        {
-            string title = titleToDelete
-                    .ToLower();
-            int removed = 0;
-            foreach (var eventToRemove in byTitle[title])
-            {
-                removed++;
-                byDate.Remove(eventToRemove);
-            }
-            byTitle.Remove(title);
-            Messages.EventDeleted(removed);
-        }
-        public void ListEvents(DateTime date, int count)
-        {
-            OrderedBag<Event>.View
-                    eventsToShow = byDate.RangeFrom(new Event(
-                    date, "", ""),
-                    true);
-            int showed = 0;
-            foreach (var eventToShow in eventsToShow)
-            {
-                if (showed == count) break;
-                Messages.PrintEvent(eventToShow);
-
-                showed++;
-            }
-            if (showed == 0) Messages.NoEventsFound();
-        }
-    }
+ 
     static EventHolder events = new EventHolder();
 
     static void Main(string[] args)
@@ -172,10 +53,7 @@ class Program
         count);
     }
 
-    private static void
-
-
-        DeleteEvents(string command)
+    private static void DeleteEvents(string command)
     {
         string title = command.Substring
         (
