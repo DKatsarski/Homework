e8c50577ca7c31b4c88d67a6e0eede24c8860c93
DKatsarski <dd.katsarski@gmail.com>
2016-09-03 02:33:04 +0300

    First Task done!

---
+++ WebTrack/05. JavaScript OOP/01. Functions/Task-1/task-1.js
@@ -0,0 +1,56 @@
+/* Task Description */
+/* 
+	Write a function that sums an array of numbers:
+		numbers must be always of type Number
+		returns `null` if the array is empty
+		throws Error if the parameter is not passed (undefined)
+		throws if any of the elements is not convertible to Number	
+*/
+
+function sum(array1, array2) {
+    
+    typeValidator(array1);
+    typeValidator(array2);
+
+    checkIfArrayIsEmpty(array1);
+    checkIfArrayIsEmpty(array2);
+
+    numbersInArrayValidator(array1);
+    numbersInArrayValidator(array2);
+
+    var sumofNumbersInArray1 = sumInArray(array1);
+    var sumofNumbersInArray2 = sumInArray(array2);
+
+	var sumArrays = sumofNumbersInArray1 + sumofNumbersInArray2;
+    return sumArrays;
+}
+console.log(sum([3, 'a', 3], [2, 2, 2]));
+
+function sumInArray(args) {
+    var temp = 0;
+    for (var i = 0; i < args.length; i++) {
+        temp += +args[i];
+    }
+    return temp;
+}
+
+function typeValidator(args) {
+  if (!Array.isArray(args)) {
+      throw new Error();
+  }
+}
+
+function checkIfArrayIsEmpty(args) {
+    if (args.length === 0) {
+        return null;
+    }
+}
+
+function numbersInArrayValidator(args) {
+   for (var i = 0; i < args.length; i++) {
+       if (isNaN(args[i])) {
+           throw new Error();
+       } 
+   }
+}
+//module.exports = sum;
