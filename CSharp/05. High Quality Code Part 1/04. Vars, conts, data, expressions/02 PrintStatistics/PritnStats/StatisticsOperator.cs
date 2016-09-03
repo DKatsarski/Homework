17b82e3e2966287e4bf350e8efcc3bc17b79b446
DKatsarski <dd.katsarski@gmail.com>
2016-09-03 15:41:13 +0300

    HW task 2 started

---
+++ CSharp/05. High Quality Code Part 1/04. Vars, conts, data, expressions/02 PrintStatistics/PritnStats/StatisticsOperator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PritnStats
+{
+    public class StatisticsOperator
+    {
+        public void PrintStatistics(double[] arr, int count)
+        {
+            double max, tmp;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            PrintMax(max);
+            tmp = 0;
+            max = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] < max)
+                {
+                    max = arr[i];
+                }
+            }
+            PrintMin(max);
+
+            tmp = 0;
+            for (int i = 0; i < count; i++)
+            {
+                tmp += arr[i];
+            }
+            PrintAvg(tmp / count);
+        }
+    }
+}
