03c13d60d7e28cd1ac8a6a0cc10b67faa76dc13f
DKatsarski <dd.katsarski@gmail.com>
2016-09-15 02:08:10 +0300

    Added some asserts to check preconditions.

--- CSharp/06. High Quality Code Part 2/Assertions-Homework/AssertionsHomework.cs
+++ CSharp/06. High Quality Code Part 2/Assertions-Homework/AssertionsHomework.cs
@@ -6,16 +6,25 @@ class AssertionsHomework
 {
     public static void SelectionSort<T>(T[] arr) where T : IComparable<T>
     {
-        for (int index = 0; index < arr.Length-1; index++)
+        // Preconditions here. They are implemented by using Assert
+        // (although it should be done with Exceptions)due to the specifics of the task in the HW.
+        Debug.Assert(arr != null, "arr can't be null!");
+        Debug.Assert(arr.Length > 1, "arr should contain at least 2 elements in order to be sorted!");
+
+
+        for (int index = 0; index < arr.Length - 1; index++)
         {
             int minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
             Swap(ref arr[index], ref arr[minElementIndex]);
         }
     }
-  
-    private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex) 
+
+    private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex)
         where T : IComparable<T>
     {
+        Debug.Assert(startIndex >= 0 && startIndex < arr.Length - 1, "startIndex cannot be lower than 0 or bigger then arr.length");
+        Debug.Assert(startIndex <= endIndex, "startIndex cannot be bigger than endIndex");
+
         int minElementIndex = startIndex;
         for (int i = startIndex + 1; i <= endIndex; i++)
         {
@@ -39,7 +48,7 @@ class AssertionsHomework
         return BinarySearch(arr, value, 0, arr.Length - 1);
     }
 
-    private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex) 
+    private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex)
         where T : IComparable<T>
     {
         while (startIndex <= endIndex)
@@ -54,7 +63,7 @@ class AssertionsHomework
                 // Search on the right half
                 startIndex = midIndex + 1;
             }
-            else 
+            else
             {
                 // Search on the right half
                 endIndex = midIndex - 1;
@@ -72,8 +81,9 @@ class AssertionsHomework
         SelectionSort(arr);
         Console.WriteLine("sorted = [{0}]", string.Join(", ", arr));
 
-        SelectionSort(new int[0]); // Test sorting empty array
-        SelectionSort(new int[1]); // Test sorting single element array
+        // These two lines are commented, so that the code could be run.
+        //SelectionSort(new int[0]); // Test sorting empty array
+        //SelectionSort(new int[1]); // Test sorting single element array
 
         Console.WriteLine(BinarySearch(arr, -1000));
         Console.WriteLine(BinarySearch(arr, 0));
