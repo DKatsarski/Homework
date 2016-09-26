041c6354b66c8c0d389d3720ab6059360a1ece88
DKatsarski <dd.katsarski@gmail.com>
2016-09-26 12:17:50 +0300

    Changes

--- CSharp/06. High Quality Code Part 2/04. Refactoring/Matrica.cs
+++ CSharp/06. High Quality Code Part 2/04. Refactoring/Matrica.cs
@@ -7,31 +7,51 @@ namespace Task3
         static void change(ref int dx, ref int dy)
         {
             int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
-
-
             int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
             int cd = 0;
+
             for (int count = 0; count < 8; count++)
-                if (dirX[count] == dx && dirY[count] == dy) { cd = count; break; }
-            if (cd == 7) { dx = dirX[0]; dy = dirY[0]; return; }
+                if (dirX[count] == dx && dirY[count] == dy)
+                {
+                    cd = count;
+                    break;
+                }
+
+            if (cd == 7)
+            {
+                dx = dirX[0]; dy = dirY[0];
+                return;
+            }
+
             dx = dirX[cd + 1];
-
-
             dy = dirY[cd + 1];
         }
+
         static bool proverka(int[,] arr, int x, int y)
         {
             int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
             int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
             for (int i = 0; i < 8; i++)
             {
-                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0) dirX[i] = 0;
+                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0)
+                {
+                    dirX[i] = 0;
+                }
 
-
-                if (y + dirY[i] >= arr.GetLength(0) || y + dirY[i] < 0) dirY[i] = 0;
+                if (y + dirY[i] >= arr.GetLength(0) || y + dirY[i] < 0)
+                {
+                    dirY[i] = 0;
+                }
             }
+
             for (int i = 0; i < 8; i++)
-                if (arr[x + dirX[i], y + dirY[i]] == 0) return true;
+            {
+                if (arr[x + dirX[i], y + dirY[i]] == 0)
+                {
+                    return true;
+                }
+            }
 
             return false;
         }
