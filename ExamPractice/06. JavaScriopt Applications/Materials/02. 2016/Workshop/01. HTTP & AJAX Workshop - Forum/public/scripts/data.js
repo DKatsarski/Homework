15eedeedc65b12891111b662615c40e4bf9ad74c
DKatsarski <dd.katsarski@gmail.com>
2016-10-04 03:15:24 +0300

    s

--- ExamPractice/06. JavaScriopt Applications/Materials/02. 2016/Workshop/01. HTTP & AJAX Workshop - Forum/public/scripts/data.js
+++ ExamPractice/06. JavaScriopt Applications/Materials/02. 2016/Workshop/01. HTTP & AJAX Workshop - Forum/public/scripts/data.js
@@ -13,7 +13,7 @@ var data = (function () {
   }
 
   function userGetCurrent() {
-    return Promise.resolve(localStorage.getItem(USERNAME_STORAGE_KEY));
+    return Promise.resolve(localStorage.getItem(USERNAME_STORAGE_KEY) || 'anonimous');
   }
   // end users
 
@@ -27,22 +27,61 @@ var data = (function () {
   }
 
   function threadsAdd(title) {
-    
+    return new Promise((resolve, reject) => {
+      let username = userGetCurrent()
+        .then((username) => {
+          let body = { title, username };
+
+          $.ajax({
+            type: 'POST',
+            url: 'api/threads',
+            data: JSON.stringify(body),
+            contentType: 'application/json',
+          }).done((data) => resolve(data))
+            .fail((err) => reject(err));
+        })
+    });
   }
 
   function threadById(id) {
-
+    return new Promise((resolve, reject) => {
+      $.getJSON(`api/threads/${id}`)
+        .done(resolve)
+        .fail(reject);
+    })
   }
 
   function threadsAddMessage(threadId, content) {
+    return new Promise((resolve, reject) => {
+      userGetCurrent()
+        .then((username) => {
+          let body = { content, username };
 
+          $.ajax({
+            type: 'POST',
+            url: `api/threads/${threadId}/messages`,
+            data: JSON.stringify(body),
+            contentType: 'application/json'
+          }).done(resolve)
+            .fail(reject);
+        })
+    });
   }
   // end threads
 
   // start gallery
   function galleryGet() {
     const REDDIT_URL = `https://www.reddit.com/r/aww.json?jsonp=?`;
-    
+
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        type: 'GET',
+        url: REDDIT_URL,
+        dataType: 'jsonp',
+        jsonpCallback: 'foo',
+      }).done(resolve)
+        .fail(reject);
+    })
   }
   // end gallery
 
