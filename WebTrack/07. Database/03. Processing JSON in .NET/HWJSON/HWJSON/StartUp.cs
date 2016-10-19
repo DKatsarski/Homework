93e7416fb5643fc03e4b8a118e6c0d7bb02e438a
DKatsarski <dd.katsarski@gmail.com>
2016-10-19 18:59:19 +0300

    HW Started. File extracted

---
+++ WebTrack/07. Database/03. Processing JSON in .NET/HWJSON/HWJSON/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWJSON
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw";
+            var fileName = "youtubeRSS";
+            WebClient myClient = new WebClient();
+            myClient.DownloadFile(url, fileName);
+
+
+        }
+    }
+}
