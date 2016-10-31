e65b952da4d2a1da0f98276d72e9e772ce677891
DKatsarski <dd.katsarski@gmail.com>
2016-10-31 17:07:43 +0200

    Entity Framework

---
+++ WebTrack/07. Database/10. EntityFramework/EntitiyFramework.Tasks/DAO.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using EntityFramework.Data;
+
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+
+namespace EntitiyFramework.Tasks
+{
+    public class DAO
+    {
+        public static NorthwindEntities GetContext()
+        {
+            var dbContext = new NorthwindEntities();
+            return dbContext;
+        }
+
+        /// <summary>
+        /// Seccond change is saved.
+        /// </summary>
+        public static void ConcurrentChanges()
+        {
+            var contextA = DAO.GetContext();
+            var contextB = DAO.GetContext();
+
+            var customerA = contextA.Customers.FirstOrDefault();
+            var customerB = contextB.Customers.FirstOrDefault();
+
+            customerA.ContactName = "customer A";
+            customerB.ContactName = "customer B";
+
+            contextA.SaveChanges();
+            contextB.SaveChanges();
+        }
+
+        public static Customer GetById(string id)
+        {
+            var context = DAO.GetContext();
+            var customer = context.Customers.FirstOrDefault(c => c.CustomerID == id);
+            return customer;
+        }
+
+        public static void InsertCustomer(Customer customer)
+        {
+            if (customer == null)
+            {
+                throw new ArgumentNullException(nameof(customer));
+            }
+
+            var context = DAO.GetContext();
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+
+        public static void ModifyCustomer(Customer modifiedCustomer)
+        {
+            if (modifiedCustomer == null)
+            {
+                throw new ArgumentNullException(nameof(modifiedCustomer));
+            }
+
+            var context = DAO.GetContext();
+            var customerWithId = context.Customers
+                .FirstOrDefault(customer => customer.CustomerID == modifiedCustomer.CustomerID);
+
+            if (customerWithId == null)
+            {
+                throw new ArgumentException("Customer does not exist.");
+            }
+
+            var values = context.Entry(customerWithId).CurrentValues;
+            values.SetValues(modifiedCustomer);
+            context.SaveChanges();
+        }
+
+        public static void RemoveCustomer(Customer customer)
+        {
+            if (customer == null)
+            {
+                throw new ArgumentNullException(nameof(customer));
+            }
+
+            var context = DAO.GetContext();
+            var matchingCustomer = context.Customers.FirstOrDefault(c => c.CustomerID == customer.CustomerID);
+            context.Customers.Remove(matchingCustomer);
+            context.SaveChanges();
+        }
+
+        public static IEnumerable<Customer> GetCustomersWithOrderYearAndCountry(int year, string country)
+        {
+            var context = DAO.GetContext();
+            var customers = context.Orders
+                .Where(o => o.OrderDate != null)
+                .Where(o => o.OrderDate.Value.Year == year && o.ShipCountry == country)
+                .Select(o => o.Customer)
+                .Distinct()
+                .ToList();
+
+            return customers;
+        }
+
+        public static IEnumerable<Order> FindsAllSalesByRegionAndPeriod(string shipReagion, DateTime start, DateTime end)
+        {
+            var context = DAO.GetContext();
+            var orders = context.Orders
+                .Where(o => o.OrderDate.HasValue && start <= o.OrderDate && o.OrderDate <= end)
+                .Where(o => o.ShipRegion != null)
+                .Where(o => o.ShipRegion == shipReagion)
+                .ToList();
+
+            return orders;
+        }
+
+        public static void CreateNorthwindTwin()
+        {
+            var northwindTwinConnectionString = "NorthwindTwin";
+            var context = new NorthwindEntities(northwindTwinConnectionString);
+            context.Database.CreateIfNotExists();
+        }
+
+        public static void ExtendedEmployeeTesting()
+        {
+            var context = DAO.GetContext();
+            var emp = context.Employees.FirstOrDefault().EntitySetTerritories;
+        }
+
+        public static void TransferServer()
+        {
+            ServerConnection conn = new ServerConnection("Server=.;Database=Northwind;Integrated Security = true");
+            Server server = new Server(conn);
+
+            Database newdb = new Database(server, "NorthwindTwin");
+            newdb.Create();
+
+            Transfer transfer = new Transfer(server.Databases["Northwind"]);
+            transfer.CopyAllObjects = true;
+            transfer.CopyAllUsers = true;
+            transfer.Options.WithDependencies = true;
+            transfer.DestinationDatabase = newdb.Name;
+            transfer.DestinationServer = server.Name;
+            transfer.DestinationLoginSecure = true;
+            transfer.CopySchema = true;
+            transfer.CopyData = true;
+            transfer.Options.ContinueScriptingOnError = true;
+            transfer.TransferData();
+        }
+    }
+}
---
+++ WebTrack/07. Database/10. EntityFramework/EntityFramework.ConsoleApp/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+using EntityFramework.Data;
+using EntitiyFramework.Tasks;
+
+namespace EntityFramework.ConsoleApp
+{
+    public class Program
+    {
+        private const string CustomerId = "ASDFG";
+
+        public static void Main()
+        {
+            Program.InsertCustomer(Program.CustomerId, "Company");
+            Program.ModifyCustomer(Program.CustomerId);
+            Program.RemoveCustomer(Program.CustomerId);
+
+            Program.InsertCustomerWithSql(Program.CustomerId, "Company");
+            Program.ModifyCustomerWithSql(Program.CustomerId);
+            Program.RemoveCustomerWithSql(Program.CustomerId);
+
+            Program.CustomersWithOrdersFromYearToCountry(1997, "Canada");
+            Program.CustomersWithOrdersFromYearToCountryWithSQL(1997, "Canada");
+
+            var end = new DateTime(2000, 6, 1);
+            var start = new DateTime(1996, 6, 1);
+            Program.FindsAllSalesByRegionAndPeriod("Essex", start, end);
+
+            DAO.CreateNorthwindTwin();
+
+            DAO.ConcurrentChanges();
+        }
+
+        private static void InsertCustomerWithSql(string id, string companyName)
+        {
+            var query =
+                @"INSERT INTO Customers (CustomerID, CompanyName)
+                        VALUES(@id, @company)";
+
+            var context = DAO.GetContext();
+            context.Database.ExecuteSqlCommand(
+                query,
+                new SqlParameter("@id", id),
+                new SqlParameter("@company", companyName));
+        }
+
+        private static void ModifyCustomerWithSql(string id)
+        {
+            var query =
+                @"UPDATE Customers
+                    SET CompanyName = @company
+                    WHERE CustomerID = @id";
+
+            var context = DAO.GetContext();
+            context.Database.ExecuteSqlCommand(
+                query,
+                new SqlParameter("@id", id),
+                new SqlParameter("@company", "changed"));
+        }
+
+        private static void RemoveCustomerWithSql(string id)
+        {
+            var query =
+                @"DELETE FROM Customers
+                    WHERE CustomerID = @id";
+
+            var context = DAO.GetContext();
+            context.Database.ExecuteSqlCommand(query, new SqlParameter("@id", id));
+        }
+
+        private static void InsertCustomer(string id, string companyName)
+        {
+            var customer = new Customer()
+            {
+                CustomerID = id,
+                CompanyName = companyName
+            };
+
+            DAO.InsertCustomer(customer);
+        }
+
+        private static void ModifyCustomer(string id)
+        {
+            var customer = DAO.GetById(id);
+            customer.ContactName = "contact name";
+            DAO.ModifyCustomer(customer);
+        }
+
+        private static void RemoveCustomer(string id)
+        {
+            var customer = DAO.GetById(id);
+            DAO.RemoveCustomer(customer);
+        }
+
+        private static void CustomersWithOrdersFromYearToCountry(int year, string country)
+        {
+            var customers = DAO.GetCustomersWithOrderYearAndCountry(year, country);
+
+            System.Console.WriteLine($"Customers with NativeSQL, OrderYear: {year}, Country: {country}");
+            foreach (var customer in customers)
+            {
+                System.Console.WriteLine($"Name: {customer.ContactName}, Country: {customer.Country}");
+            }
+
+            System.Console.WriteLine();
+        }
+
+        private static void CustomersWithOrdersFromYearToCountryWithSQL(int year, string country)
+        {
+            var queryTemplate = @"SELECT *
+                    FROM Customers c
+                    JOIN Orders o
+                        ON o.CustomerID = c.CustomerID
+                    WHERE o.ShipCountry = '{1}' AND YEAR(o.OrderDate) = {0}";
+            var query = string.Format(queryTemplate, year, country);
+
+            var context = new NorthwindEntities();
+            var customers = context.Customers.SqlQuery(query).Distinct().ToList();
+
+            System.Console.WriteLine($"Customers with NativeSQL, OrderYear: {year}, Country: {country}");
+            foreach (var customer in customers)
+            {
+                System.Console.WriteLine($"Name: {customer.ContactName}, Country: {customer.Country}");
+            }
+
+            System.Console.WriteLine();
+        }
+
+        private static void FindsAllSalesByRegionAndPeriod(string shipReagion, DateTime start, DateTime end)
+        {
+            var orders = DAO.FindsAllSalesByRegionAndPeriod(shipReagion, start, end);
+
+            System.Console.WriteLine($"Orders between, Start: {start.ToShortDateString()}, End: {end.ToShortDateString()}, Region: {shipReagion}");
+            foreach (var order in orders)
+            {
+                System.Console.WriteLine($"Name: {order.OrderDate}, Country: {order.ShipRegion}");
+            }
+
+            System.Console.WriteLine();
+        }
+    }
+}
