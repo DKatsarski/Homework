e4e6b96b02c79f4b345466c7fcee38dc0839e7dc
DKatsarski <dd.katsarski@gmail.com>
2016-11-03 23:58:29 +0200

    HW started. Entities connected.

---
+++ WebTrack/07. Database/10. EntityFramework/HW Redone/EntityFrameworkHW/EntityFrameworkHW/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkHW
+{
+    public class Startup
+    {
+        static void Main()
+        {
+            using (var db = new NorthwindEntities())
+            {
+                var project = db.Regions
+                    .ToList();
+                Console.WriteLine(project.Count);
+            }
+        }
+    }
+}
