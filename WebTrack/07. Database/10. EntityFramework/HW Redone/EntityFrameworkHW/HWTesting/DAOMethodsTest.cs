0a86fd56cedc8052957df2fa4b3c1fcb63f3ddee
DKatsarski <dd.katsarski@gmail.com>
2016-11-04 21:35:13 +0200

    no idea how to test a db

---
+++ WebTrack/07. Database/10. EntityFramework/HW Redone/EntityFrameworkHW/HWTesting/DAOMethodsTest.cs
@@ -0,0 +1,26 @@
+﻿using EntityFrameworkHW;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWTesting
+{
+    [TestFixture]
+    public class DAOMethodsTest
+    {
+        [Test]
+        public void TestInseartMethod()
+        {
+
+            var context = new Mock<Customer>();
+
+
+
+
+        }
+    }
+}
