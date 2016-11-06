d6912e898b63d546dab6d67572c88548399798db
DKatsarski <dd.katsarski@gmail.com>
2016-11-06 23:54:29 +0200

    More attributes added.

--- WebTrack/07. Database/11. EntityFrameworkCodeFirst/HWRedone/CodeFirstHWRedone/StudentSystem.Models/Course.cs
+++ WebTrack/07. Database/11. EntityFrameworkCodeFirst/HWRedone/CodeFirstHWRedone/StudentSystem.Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,12 @@ namespace StudentSystem.Models
 
         public Guid Id { get; set; }
 
+        [Required]
+        [MinLength(4)]
+        [MaxLength(54)]
         public string Name { get; set; }
 
+        
         public string Description { get; set; }
 
         public string Materials { get; set; }
