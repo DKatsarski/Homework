f6345aa3dac4a5e118b49f45b7729ab0993c6a02
DKatsarski <dd.katsarski@gmail.com>
2016-11-06 23:44:23 +0200

    New attributes added. using System.ComponentModel.DataAnnotations.Schema introduced.

--- WebTrack/07. Database/11. EntityFrameworkCodeFirst/HWRedone/CodeFirstHWRedone/StudentSystem.Models/Student.cs
+++ WebTrack/07. Database/11. EntityFrameworkCodeFirst/HWRedone/CodeFirstHWRedone/StudentSystem.Models/Student.cs
@@ -4,19 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StudentSystem.Models
 {
     public class Student
     {
-        
+
 
         public int Id { get; set; }
 
         [Required]
-        [StringLength(50, MinimumLength =10)]
+        [StringLength(50, MinimumLength = 2)]
         public string Name { get; set; }
 
+        [Index(IsClustered = false, IsUnique = true)]
+        [StringLength(10, MinimumLength = 10)]
         public int Number { get; set; }
 
         public Guid HomeworkId { get; set; }
