dbf779ef3e654925f63ea645f7cf206044f4e6a5
DKatsarski <dd.katsarski@gmail.com>
2016-11-06 20:32:07 +0200

    DB Context implemented

--- WebTrack/07. Database/11. EntityFrameworkCodeFirst/HWRedone/CodeFirstHWRedone/StudentSystem.Data/StudentDbContext.cs
+++ WebTrack/07. Database/11. EntityFrameworkCodeFirst/HWRedone/CodeFirstHWRedone/StudentSystem.Data/StudentDbContext.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using StudentSystem.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudentSystem.Data
 {
-    public class Class1
+    public class StudentDbContext : DbContext
     {
+        public virtual IDbSet<Student> Students { get; set; }
+
+        public virtual IDbSet<Homework> Homeworks { get; set; }
+
+        public virtual IDbSet<Course> Courses { get; set; }
+
+        public virtual IDbSet<StudetsInCourse> StudentsInCourses { get; set; }
     }
 }
