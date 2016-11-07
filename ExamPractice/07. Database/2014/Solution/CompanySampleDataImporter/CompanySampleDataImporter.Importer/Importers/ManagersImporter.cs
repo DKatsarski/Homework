a2aefc780b87140580aed559c18d0fcbc5c21246
DKatsarski <dd.katsarski@gmail.com>
2016-11-08 00:00:05 +0200

    Started work on the ManagersImporter.cs.

---
+++ ExamPractice/07. Database/2014/Solution/CompanySampleDataImporter/CompanySampleDataImporter.Importer/Importers/ManagersImporter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompanySampleDataImporter.Data;
+
+namespace CompanySampleDataImporter.Importer.Importers
+{
+    class ManagersImporter : IImporter
+    {
+        public Action<CompanyEntities, TextWriter> Get
+        {
+            get
+            {
+
+                return (db, tr) =>
+                {
+
+                };
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return "Importing Managers";
+            }
+        }
+
+        public int Order
+        {
+            get
+            {
+                return 3;
+            }
+        }
+    }
+}
