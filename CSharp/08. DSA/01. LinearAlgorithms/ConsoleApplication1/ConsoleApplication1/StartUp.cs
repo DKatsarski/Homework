bf076c464ed936198fa90c91c35fcda300da5a28
DKatsarski <dd.katsarski@gmail.com>
2016-11-23 19:36:29 +0200

    First Task done! The easy way.

--- CSharp/08. DSA/01. LinearAlgorithms/ConsoleApplication1/ConsoleApplication1/StartUp.cs
+++ CSharp/08. DSA/01. LinearAlgorithms/ConsoleApplication1/ConsoleApplication1/StartUp.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class StartUp
     {
-        static void Main(string[] args)
+        /*
+         Write a program that reads from the console a sequence of positive integer numbers.
+
+            The sequence ends when empty line is entered.
+            Calculate and print the sum and average of the elements of the sequence.
+            Keep the sequence in List<int>. 
+         */
+        static void Main()
         {
+            List<int> list = new List<int>();
+
+            while (true)
+            {
+                var n = Console.ReadLine();
+                if (string.IsNullOrEmpty(n))
+                {
+                    break;
+                }
+
+                list.Add(int.Parse(n));
+            }
+            var sum = list.Sum();
+            double average = list.Average();
+            Console.WriteLine(sum);
+            Console.WriteLine("{0:F2}", average);
         }
     }
 }
