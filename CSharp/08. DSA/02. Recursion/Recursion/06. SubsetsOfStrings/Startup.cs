9c3bc473d60e77c31b3e8188fa14f87fc5e5f532
DKatsarski <dd.katsarski@gmail.com>
2016-11-30 19:47:56 +0200

    Task 6 almost done

---
+++ CSharp/08. DSA/02. Recursion/Recursion/06. SubsetsOfStrings/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.CombinationsWithDuplicants
+{
+    public class Startup
+    {
+        static void Main()
+        {
+            var list = new List<string>() { "test", "rock", "fun" };
+
+            int permutationsOfNumber = list.Count;
+            int lenthOfLoop = permutationsOfNumber;
+
+            NestingLoops(permutationsOfNumber, lenthOfLoop, list);
+        }
+
+        public static void NestingLoops(int n, int loopNumber, List<string> list, string vector = "", int counter = 0)
+        {
+            if (n == 1)
+            {
+
+                var stringWithNoWhiteSpaces = string.Join(" ", vector.Trim()).Split(' ');
+
+                // Cheking if the same number exists two times
+                for (int i = 0; i < stringWithNoWhiteSpaces.Length - 1; i++)
+                {
+
+                    if (stringWithNoWhiteSpaces[i] == stringWithNoWhiteSpaces[i + 1])
+                    {
+                        return;
+                    }
+                }
+
+                // I can put them all  in a list and then do a seperate check to see if something is repeating
+                Console.Write("(" + vector.Trim() + "), ");
+                return;
+            }
+
+            NestingLoops(n - 1, loopNumber, list, vector + " " + list[counter]);
+
+            for (int i = 0; i < loopNumber - 1; i++)
+            {
+                if (counter >= list.Count - 1)
+                {
+                    return;
+                }
+                else
+                {
+                    NestingLoops(n - 1, loopNumber, list, vector + " " + list[counter += 1]);
+                }
+            }
+        }
+    }
+}
--- CSharp/08. DSA/02. Recursion/Recursion/Subset/Startup.cs
+++ CSharp/08. DSA/02. Recursion/Recursion/Subset/Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace Subset
 {
-    class Startup
+    public class Startup
     {
         private static IList<string> elementsToDisplay;
 
