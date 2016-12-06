130afe369a3203ddb6111f1aa5e7e233713385aa
DKatsarski <dd.katsarski@gmail.com>
2016-12-06 14:28:58 +0200

    First task done!

---
+++ CSharp/08. DSA/03. Combinatorics/Combinatorics/Combinatorics/Counter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinatorics
+{
+    public static class Counter
+    {
+        public static List<int> CountMe(int number)
+        {
+            List<int> list = new List<int>();
+
+
+            list.Add(number);
+
+
+            return list;
+        }
+    }
+}
--- CSharp/08. DSA/03. Combinatorics/Combinatorics/Combinatorics/Program.cs
+++ CSharp/08. DSA/03. Combinatorics/Combinatorics/Combinatorics/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Combinatorics
 {
     public class Program
     {
+        static void CountingCombinations(int n, List<int> list, string vector = "")
+        {
+            if (n == 0)
+            {
+
+                list.Add(1);
+                return;
+            }
+
+            var counter = 1;
+
+            CountingCombinations(n - 1, list, vector + counter);
+
+            CountingCombinations(n - 1, list, vector + (counter += 1));
+
+        }
         static void Main()
         {
+            string input = Console.ReadLine();
+            var counter = 1;
+            char ch = '*';
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == ch)
+                {
+                    counter++;
+                }
+            }
+            
+            if (counter == 1)
+            {
+                Console.WriteLine(counter);
+            }
+            else
+            {
+                int number = counter;
+                var list = new List<int>();
+
+                CountingCombinations(number, list);
+                Console.WriteLine(list.Count);
+            }
         }
     }
 }
