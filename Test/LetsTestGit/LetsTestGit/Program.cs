2498d2e54816a285204a80239b8114799c64e0a7
DKatsarski <dd.katsarski@gmail.com>
2017-04-25 23:07:34 -0400

    Test

---
+++ Test/LetsTestGit/LetsTestGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LetsTestGit
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("hello again, old friend");
+        }
+    }
+}
