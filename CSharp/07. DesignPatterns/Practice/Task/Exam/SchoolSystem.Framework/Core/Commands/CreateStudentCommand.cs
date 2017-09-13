be56213fe9c2dbf1b5841ee8052e36a5dcb2ce8c
DKatsarski <dd.katsarski@gmail.com>
2017-09-13 16:17:33 +0300

    CreateStudentCommand no longer uses "new" in any instance

--- CSharp/07. DesignPatterns/Practice/Task/Exam/SchoolSystem.Framework/Core/Commands/CreateStudentCommand.cs
+++ CSharp/07. DesignPatterns/Practice/Task/Exam/SchoolSystem.Framework/Core/Commands/CreateStudentCommand.cs
@@ -3,6 +3,7 @@ using SchoolSystem.Framework.Core.Commands.Contracts;
 using SchoolSystem.Framework.Models;
 using SchoolSystem.Framework.Models.Enums;
 using SchoolSystem.Framework.Models.Contracts;
+using SchoolSystem.Framework.Core.Contracts;
 
 namespace SchoolSystem.Framework.Core.Commands
 {
@@ -10,25 +11,36 @@ namespace SchoolSystem.Framework.Core.Commands
     {
         private static int currentStudentId = 0;
 
+        private readonly IStudentFactory studentFactory;
+        private readonly IAddStudent addStudent;
+
+        public CreateStudentCommand(IStudentFactory studentFactory, IAddStudent addStudent)
+        {
+            this.studentFactory = studentFactory;
+            this.addStudent = addStudent;
+        }
+
         public string Execute(IList<string> parameters)
         {
             var firstName = parameters[0];
             var lastName = parameters[1];
             var grade = (Grade)int.Parse(parameters[2]);
 
-            var student = new Student(firstName, lastName, grade);
-            Engine.Students.Add(currentStudentId, student);
+
+
+            var student = this.studentFactory.CreateStudent(firstName, lastName, grade); // new Student(firstName, lastName, grade);
+            this.addStudent.AddStudent(currentStudentId, student); //Engine.Students.Add(currentStudentId, student);
 
             return $"A new student with name {firstName} {lastName}, grade {grade} and ID {currentStudentId++} was created.";
         }
     }
 
-    public interface IStudent
-    {
-        IStudent CreateStudent(string firstName, string lastName, Grade grade);
+    //public interface IStudent
+    //{
+    //    IStudent CreateStudent(string firstName, string lastName, Grade grade);
 
-        ITeacher CreateTeacher(string firstName, string lastName, Subject subject);
+    //    ITeacher CreateTeacher(string firstName, string lastName, Subject subject);
 
-        IMark CreateMark(Subject subject, float value);
-    }
+    //    IMark CreateMark(Subject subject, float value);
+    //}
 }
