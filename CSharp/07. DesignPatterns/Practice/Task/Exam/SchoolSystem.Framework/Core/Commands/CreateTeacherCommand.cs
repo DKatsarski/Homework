03edaf508e5eb6246735550367162e5f5977a24f
DKatsarski <dd.katsarski@gmail.com>
2017-09-13 17:07:53 +0300

    teacherFactory added. Commands/CreateTeacherCommand.cs updated

--- CSharp/07. DesignPatterns/Practice/Task/Exam/SchoolSystem.Framework/Core/Commands/CreateTeacherCommand.cs
+++ CSharp/07. DesignPatterns/Practice/Task/Exam/SchoolSystem.Framework/Core/Commands/CreateTeacherCommand.cs
@@ -3,6 +3,8 @@
 using SchoolSystem.Framework.Core.Commands.Contracts;
 using SchoolSystem.Framework.Models;
 using SchoolSystem.Framework.Models.Enums;
+using SchoolSystem.Framework.Core.Contracts;
+using SchoolSystem.Framework.Models.Contracts;
 
 namespace SchoolSystem.Framework.Core.Commands
 {
@@ -10,14 +12,23 @@ namespace SchoolSystem.Framework.Core.Commands
     {
         private static int currentTeacherId = 0;
 
+        private readonly ITeacherFactory teacherFactory;
+        private readonly IAddTeacher addTeacher;
+
+        public CreateTeacherCommand(ITeacherFactory teacherFactory, IAddTeacher addTeacher)
+        {
+            this.teacherFactory = teacherFactory;
+            this.addTeacher = addTeacher;
+        }
+
         public string Execute(IList<string> parameters)
         {
             var firstName = parameters[0];
             var lastName = parameters[1];
             var subject = (Subject)int.Parse(parameters[2]);
 
-            var teacher = new Teacher(firstName, lastName, subject);
-            Engine.Teachers.Add(currentTeacherId, teacher);
+            var teacher = this.teacherFactory.CreateTeacher(firstName, lastName, subject); // new Teacher(firstName, lastName, subject);
+            this.addTeacher.AddTeacher(currentTeacherId, teacher); //Engine.Teachers.Add(currentTeacherId, teacher);
 
             return $"A new teacher with name {firstName} {lastName}, subject {subject} and ID {currentTeacherId++} was created.";
         }
---
+++ CSharp/07. DesignPatterns/Practice/Task/Exam/SchoolSystem.Framework/Models/Contracts/IAddTeacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Framework.Models.Contracts
+{
+    public interface IAddTeacher
+    {
+        void AddTeacher(int teacherId, ITeacher teacher);
+    }
+}
