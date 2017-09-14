7465b81d5784a23c88e1b0d612d6b4e640028020
DKatsarski <dd.katsarski@gmail.com>
2017-09-14 15:37:56 +0300

    Implemented interfaces and removes engine instances in the RemoveTeacherCommand and RemoveStudentCommand

--- CSharp/07. DesignPatterns/Practice/Task/Exam/SchoolSystem.Framework/Core/Commands/RemoveTeacherCommand.cs
+++ CSharp/07. DesignPatterns/Practice/Task/Exam/SchoolSystem.Framework/Core/Commands/RemoveTeacherCommand.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 
 using SchoolSystem.Framework.Core.Commands.Contracts;
+using SchoolSystem.Framework.Models.Contracts;
 
 namespace SchoolSystem.Framework.Core.Commands
 {
     public class RemoveTeacherCommand : ICommand
     {
+        private readonly IRemoveTeacher removeTeacher;
+
+        public RemoveTeacherCommand(IRemoveTeacher removeTeacher)
+        {
+            this.removeTeacher = removeTeacher;
+        }
+
+        
         public string Execute(IList<string> parameters)
         {
             var teacherId = int.Parse(parameters[0]);
@@ -16,7 +25,8 @@ namespace SchoolSystem.Framework.Core.Commands
                 throw new ArgumentException("The given key was not present in the dictionary.");
             }
 
-            Engine.Teachers.Remove(teacherId);
+            removeTeacher.RemoveTeacher(teacherId);
+
             return $"Teacher with ID {teacherId} was sucessfully removed.";
         }
     }
