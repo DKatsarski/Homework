84892a3bce7df0c73869ce7c88d7ad2c95cdc705
DKatsarski <dd.katsarski@gmail.com>
2017-10-03 12:22:09 +0300

    added comments

--- CSharp/07. DesignPatterns/Practice/Task/Exam/SchoolSystem.CLI/Startup.cs
+++ CSharp/07. DesignPatterns/Practice/Task/Exam/SchoolSystem.CLI/Startup.cs
@@ -14,7 +14,7 @@ namespace SchoolSystem.Cli
             var engine = kernel.Get<Engine>();
 
             engine.Start();
-            // 4:45:00
+            // 5:02;00 - the measurmens are starting 
         }
     }
 }
