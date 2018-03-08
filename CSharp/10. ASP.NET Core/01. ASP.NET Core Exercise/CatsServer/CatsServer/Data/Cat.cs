c42d761a803e5368158aa1dc9313b1157705495b
DKatsarski <dd.katsarski@gmail.com>
2018-03-08 22:49:22 +0200

    Added migration

---
+++ CSharp/10. ASP.NET Core/01. ASP.NET Core Exercise/CatsServer/CatsServer/Data/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatsServer.Data
+{
+    public class Cat
+    {
+        private const int StringMaxLength = 50;
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(StringMaxLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, 30)]
+        public int Age { get; set; }
+
+        [Required]
+        [MaxLength(StringMaxLength)]
+        public string Breed { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string ImageUrl { get; set; }
+    }
+}
--- CSharp/10. ASP.NET Core/01. ASP.NET Core Exercise/CatsServer/CatsServer/Startup.cs
+++ CSharp/10. ASP.NET Core/01. ASP.NET Core Exercise/CatsServer/CatsServer/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CatsServer.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CatsServer
@@ -15,6 +17,8 @@ namespace CatsServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CatsDbContext>(options =>
+            options.UseSqlServer("Server=.;Database=CatsServerDb; Integrated Security=True"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
