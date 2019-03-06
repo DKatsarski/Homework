ef7ea681619dd9504b8d8a8e78845e183eb32dd7
DKatsarski <dd.katsarski@gmail.com>
2019-03-06 15:00:41 +0200

    Generated a method

---
+++ CSharp/02. CSharp2/Practice/PracticeCSharp2/PracticeCSharp2/NumberPrinter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCSharp2
+{
+    public static class NumberPrinter
+    {
+        public static void PrintUpToANumber(int number)
+        {
+            for (int i = 0; i < number + 1; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ CSharp/02. CSharp2/Practice/PracticeCSharp2/PracticeCSharp2/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCSharp2
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+            NumberPrinter.PrintUpToANumber(5);
+
+        }
+
+      
+    }
+}
