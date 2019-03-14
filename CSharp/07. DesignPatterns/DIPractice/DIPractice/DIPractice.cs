c84e6edf2b598998e6a8793dabb1e884f5017c2b
DKatsarski <dd.katsarski@gmail.com>
2019-03-14 09:05:24 +0200

    Added DIPractice folder Practice started

---
+++ CSharp/07. DesignPatterns/DIPractice/DIPractice/DIPractice.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPractice
+{
+    public class DIPractice
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello DI");
+        }
+    }
+}
