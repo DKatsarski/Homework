24a170ef061c9e0b0469aad587b0deda2fbb3be6
DKatsarski <dd.katsarski@gmail.com>
2019-04-07 15:55:57 +0300

    Find Birthday Program

--- CSharp/02. CSharp2/Practice/PracticeCSharp2/PracticeCSharp2/StartUp.cs
+++ CSharp/02. CSharp2/Practice/PracticeCSharp2/PracticeCSharp2/StartUp.cs
@@ -4,60 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Problem_06.String_length
+namespace PracticeCSharp2
 {
-    class Program
+    public static class StartUp
     {
         static void Main(string[] args)
         {
-            //Write a program that reads from the console a string of maximum 20 characters.If the length of the string is less than 20, the rest of the characters should be filled with *.
-            //Print the result string into the console.Write a program that reads from the console a string of maximum 20 characters.If the length of the string is less than 20, the rest of the characters should be filled with *.
-            //Print the result string into the console.
-
-            //substring - index to 20 
-
-            char[] arr = new char[20];
-            int counterPosition = 0;
-            int counterLenght = 0;
-            char currentKey = Char.MinValue;
-            char homo = Char.MaxValue;
-
-            do
-            {
-                currentKey = Console.ReadKey().KeyChar;
-
-                if (currentKey == (char)ConsoleKey.Enter)
-                {
-                    homo = currentKey;
-                }
-                else
-                {
-                    arr[counterPosition] = currentKey;
-
-                    counterLenght++;
-                    counterPosition++;
-                }
 
 
+            DateTime date = DateTime.Now;
+            DateTime birthDay = DateTime.Parse("02/14/1988");
+            var years = date.Year - birthDay.Year;
 
-            } while (homo != (char)ConsoleKey.Enter);
+            Console.WriteLine(years);
 
-            if (counterPosition != 20)
-            {
-                for (int i = counterPosition; i < arr.Length; i++)
-                {
-                    arr[i] = '*';
-                }
-
-            }
-
-
-
-            Console.WriteLine();
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
         }
     }
 }
