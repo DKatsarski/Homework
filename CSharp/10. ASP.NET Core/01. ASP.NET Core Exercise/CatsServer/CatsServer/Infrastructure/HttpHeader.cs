6b18b0cd0fa18932a0aa24df605f916c4227ce87
DKatsarski <dd.katsarski@gmail.com>
2019-04-08 17:46:47 +0300

    cleaning folder

--- CSharp/10. ASP.NET Core/01. ASP.NET Core Exercise/CatsServer/CatsServer/Infrastructure/HttpHeader.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CatsServer.Infrastructure
-{
-    public class HttpHeader
-    {
-        public const string ContetnType = "Content-Type";
-        public const string Location = "Location";
-    }
-}
