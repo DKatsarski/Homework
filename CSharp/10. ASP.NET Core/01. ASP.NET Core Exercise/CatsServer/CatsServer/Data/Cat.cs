d6b5897bd3f45d0f76f60ef3a42629942b67b244
DKatsarski <dd.katsarski@gmail.com>
2019-04-08 18:12:44 +0300

    New catsServer started.
    introduces class cat
    
    CatsDbContext.cs

---
+++ CSharp/10. ASP.NET Core/01. ASP.NET Core Exercise/CatsServer/CatsServer/Data/Cat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatsServer.Data
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+            
+        [Range(0, 30)]
+        public int Age { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Breed { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string ImageUrl { get; set; }
+
+    }
+}
