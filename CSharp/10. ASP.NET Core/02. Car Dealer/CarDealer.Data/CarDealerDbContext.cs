fe60bab4d362c5c7e7cda7ebfa9836c45becea76
DKatsarski <dd.katsarski@gmail.com>
2019-05-01 15:20:26 +0300

    A ton of changes -> CarDealer server started.

---
+++ CSharp/10. ASP.NET Core/02. Car Dealer/CarDealer.Data/CarDealerDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CarDealer.Data.Models;
+
+namespace CarDealer.Data
+{
+    public class CarDealerDbContext : IdentityDbContext<User>
+    {
+        public CarDealerDbContext(DbContextOptions<CarDealerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<Sale>()
+                .HasOne(s => s.Car)
+                .WithMany(c => c.Sales)
+                .HasForeignKey(s => s.Car_Id);
+
+            builder
+                .Entity<Sale>()
+                .HasOne(s => s.Customer)
+                .WithMany(c => c.Sales)
+                .HasForeignKey(s => s.Customer_Id);
+
+
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
