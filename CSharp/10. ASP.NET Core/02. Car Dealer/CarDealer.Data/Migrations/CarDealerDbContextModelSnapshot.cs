01017c9cac93e19f6de45f1bd859d5a169f8c44c
DKatsarski <dd.katsarski@gmail.com>
2019-05-06 21:38:35 +0300

    Somehow fixed the problem and the app is running I was missing a change of ApplicationDbContedt to CarDbContext

--- CSharp/10. ASP.NET Core/02. Car Dealer/CarDealer.Data/Migrations/CarDealerDbContextModelSnapshot.cs
+++ CSharp/10. ASP.NET Core/02. Car Dealer/CarDealer.Data/Migrations/CarDealerDbContextModelSnapshot.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace CarDealer.Data.Migrations
 {
     [DbContext(typeof(CarDealerDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    partial class CarDealerDbContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
