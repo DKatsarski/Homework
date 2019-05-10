6d14267a91cd5cd3c405d62268cc455cfe86c7db
DKatsarski <dd.katsarski@gmail.com>
2019-05-10 13:49:40 +0300

    last update on PracticeCSharp2

--- CSharp/02. CSharp2/Practice/PracticeCSharp2/PracticeCSharp2/StartUp.cs
+++ CSharp/02. CSharp2/Practice/PracticeCSharp2/PracticeCSharp2/StartUp.cs
@@ -1,44 +1,57 @@
-﻿namespace Defining_Classes_Main
+﻿namespace PracticeCSharp2
 {
     using System.Collections.Generic;
+    using System;
 
-    public struct Point3D
+    public class StartUp
     {
-        /// <summary>
-        /// Create a structure Point3D to hold a 3D-coordinate {X, Y, Z} in the Euclidian 3D space.
-        /// </summary>
 
-        private decimal X;
-        private decimal Y;
-        private decimal Z;
-
-        /// <summary>
-        /// Add a private static read-only field to hold the start of the coordinate system – the point O{0, 0, 0}.
-        /// </summary>
-        static readonly List<decimal> O = new List<decimal> { 0, 0, 0 };
-
-        public Point3D(decimal x, decimal y, decimal z)
+         static void Main()
         {
-            this.X = x;
-            this.Y = y;
-            this.Z = z;
+            var array = new int[] { 2, 1, 5, 3, 4 };
+            var newArray = new int[] { 2, 5, 1, 3, 4 };
+
+            minimumBribes(array);
+            minimumBribes(newArray);
         }
 
 
-        /// <summary>
-        /// Implement the ToString() to enable printing a 3D point.
-        /// </summary>
-        /// <returns> Printed 3D point </returns>
-        /// 
+        static void minimumBribes(int[] arr)
+        {
+            int n = arr.Length;
+            int counter = 0;
 
-        public static List<decimal> ReturnCoordinates
-        { get
+
+            if (counter <= 2)
             {
-                return O;
+
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (arr[j] > arr[j + 1])
+                        {
+                            // swap temp and arr[i] 
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                            counter += 1;
+                        }
+                    }
+                }
+                if (counter > 2)
+                {
+                    Console.WriteLine(counter);
+                    Console.WriteLine("Too chaotic");
+
+                }
+                else
+                {
+                    Console.WriteLine(counter);
+                }
             }
         }
-
     }
-  
 
 }
