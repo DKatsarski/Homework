d8ad55b442884d3d82f5aa9c1ff61deae59f0afb
DKatsarski <dd.katsarski@gmail.com>
2019-05-22 17:59:46 +0300

    StringExtensions.cs added for the repeatable code in the views Details completed

---
+++ CSharp/10. ASP.NET Core/02. Car Dealer/CarDealer.Web/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealer.Web.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        private const string NumberFormat = "F2";
+
+        public static string ToPrice(this decimal priceText)
+        {
+            return $"${priceText.ToString(NumberFormat)}";
+        }
+
+        public static string ToPercentage(this double number)
+        {
+            return $"{number.ToString(NumberFormat)}%";
+
+        }
+    }
+}
