617aaa531480e784191ce86bc51baa0a8f313c03
DKatsarski <dd.katsarski@gmail.com>
2019-05-22 15:39:50 +0300

    SaleService Implementation completed Visualization as a table to be next

--- CSharp/10. ASP.NET Core/02. Car Dealer/CarDealer.Services/Implementations/SaleService.cs
+++ CSharp/10. ASP.NET Core/02. Car Dealer/CarDealer.Services/Implementations/SaleService.cs
@@ -1,11 +1,13 @@
 ﻿using CarDealer.Data;
+using CarDealer.Services.Models.Sales;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CarDealer.Services.Implementations
 {
-    public class SaleService
+    public class SaleService : ISaleService
     {
         private readonly CarDealerDbContext db;
 
@@ -13,5 +15,17 @@ namespace CarDealer.Services.Implementations
         {
             this.db = db;
         }
+
+        public IEnumerable<SaleListModel> All()
+        => this.db
+            .Sales
+            .Select(s => new SaleListModel
+            {
+                CustomerName = s.Customer.Name,
+                Discount = s.Discount,
+                IsYoungDriver = s.Customer.IsYoungDriver,
+                Price = s.Car.Parts.Sum(p => p.Part.Price)
+            })
+            .ToList();
     }
 }
