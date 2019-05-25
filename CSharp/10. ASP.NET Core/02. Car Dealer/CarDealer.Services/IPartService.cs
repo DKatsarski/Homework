dd3870f41138479334f768ffa14e5a7c90ec8d54
DKatsarski <dd.katsarski@gmail.com>
2019-05-25 14:41:10 +0300

    PartService - implementation started

---
+++ CSharp/10. ASP.NET Core/02. Car Dealer/CarDealer.Services/IPartService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.Services
+{
+    public interface IPartService
+    {
+        void All(int page = 1);
+    }
+}
