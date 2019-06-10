24ebda507997e98508bfde9f8654a38158017242
DKatsarski <dd.katsarski@gmail.com>
2019-06-10 11:45:51 +0300

    Playing with Select(x => new...)

--- CSharp/02. CSharp2/Practice/PracticeCSharp2/PracticeCSharp2/StartUp.cs
+++ CSharp/02. CSharp2/Practice/PracticeCSharp2/PracticeCSharp2/StartUp.cs
@@ -8,18 +8,47 @@
     {
         static void Main()
         {
-            decimal asdf = 2344.234234m;
-            int a = 2;
+            var arr = new int[] { 2, 3, 4, 5};
+            
+            var experiment = arr
+                .Select(x => new int[]
+                { 
+                })
+                .ToList();
 
-            Console.WriteLine(asdf.ToPrice());
+            foreach (var num in experiment)
+            {
 
-            Console.WriteLine(a.AlittleMoreInt());
-
+                Console.WriteLine(num);
+            }
         }
 
-        public static void MyWrtier(IWriter wrtier)
+        public static int MyWrtier(int[] arr)
         {
-            wrtier.Write("Exmaple Of Interface");
+            var lagergestNumber = 0;
+            var temp = string.Empty;
+            var tempInt = 0;
+
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                for (int j = i; j < i + 3; j++)
+                {
+                    temp += arr[j];
+                }
+
+                tempInt = int.Parse(temp);
+
+                if (lagergestNumber < tempInt)
+                {
+                    lagergestNumber = tempInt;
+                }
+
+                tempInt = 0;
+                temp = string.Empty;
+            }
+
+            return lagergestNumber;
         }
     }
 }
+
